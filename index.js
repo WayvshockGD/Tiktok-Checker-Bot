0a708caa0d8e24002c579f5e3f2e3c3d936287e8
WayvshockGD <61085765+WayvshockGD@users.noreply.github.com>
2022-12-30 21:57:05 -0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,64 @@
+const Eris = require("eris");
+const fetch = require("node-fetch");
+
+// Replace these values with your own
+const BOT_TOKEN = "YOUR_BOT_TOKEN";
+const CLIENT_ID = "YOUR_CLIENT_ID";
+const CLIENT_SECRET = "YOUR_CLIENT_SECRET";
+
+// Replace these values with the IDs of the users you want to send messages to
+const USER_IDS = ["USER_ID_1", "USER_ID_2", "USER_ID_3"];
+
+// Create a Discord client
+const client = new Eris(BOT_TOKEN, {
+  // Set some client options
+  disableEveryone: true,
+  getAllUsers: true,
+});
+
+async function sendMessage() {
+  // Get the TikTok app's information from the Google Play store
+  const url =
+    "https://www.googleapis.com/androidpublisher/v3/applications/com.zhiliaoapp.musically";
+  const headers = {
+    Authorization: `Bearer ${CLIENT_SECRET}`,
+  };
+  const params = {
+    packageName: "com.zhiliaoapp.musically",
+  };
+  const response = await fetch(url, { headers, params });
+  const data = await response.json();
+
+  // Check if the TikTok app is available
+  const available = data.available;
+  let status;
+  let emoji;
+  if (available) {
+    status = "available";
+    emoji = "✅";
+  } else {
+    status = "not available";
+    emoji = "❌";
+  }
+
+  // Create an embed with the availability status
+  const embed = new Eris.RichEmbed()
+    .setTitle("TikTok App Availability")
+    .setDescription(`The TikTok app is currently ${status} on the Google Play store.`)
+    .setColor(available ? "GREEN" : "RED")
+    .setTimestamp()
+    .setFooter(emoji);
+
+  // Send the embed to the specified users
+  for (const userId of USER_IDS) {
+    const user = client.users.get(userId);
+    await user.send({ embed });
+  }
+}
+
+client.on("ready", () => {
+  // Send a message every 24 hours
+  setInterval(sendMessage, 86400);
+});
+
+client.connect();
